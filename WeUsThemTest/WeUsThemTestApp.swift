cddf21f1da3d1a16ca736394250ebca4a97e3580
SreekuttanCL <sreekuttancl12@gmail.com>
2022-01-26 09:30:25 -0400

    Initial Commit

---
+++ WeUsThemTest/WeUsThemTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeUsThemTestApp.swift
+//  WeUsThemTest
+//
+//  Created by Sreekuttan C L on 2022-01-26.
+//
+
+import SwiftUI
+
+@main
+struct WeUsThemTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
